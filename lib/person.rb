2f3a1f371e4ef3cb5cd47ad8de3a0417dd2476da
github username <github email address>
2019-09-17 16:42:10 -0400

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+
+def initialize(name)
+    @name=name
+end
+
+end
+
+
+# #### 1. `Person#initialize` with a Name
+
+# Define a `Person` class in `lib/person.rb` that provides an `#initialize` method
+# that accepts an argument for the person's name. That argument should be stored
+# within a `@name` instance variable.
